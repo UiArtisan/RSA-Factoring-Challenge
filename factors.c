1941e9e2b38804c8234ae34dd75a6888cbbd2d88
UiArtisan <artisanui@gmail.com>
2024-09-30 17:48:26 +0100

    factore factories

--- factors.c
+++ factors.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <gmp.h>
+#include <sys/types.h>
+#if defined(_WIN32) || defined(_WIN64)
+typedef long ssize_t;
+#endif
 
 /**
  * main - Entry point.
@@ -20,7 +24,7 @@ int main(int argc, char *argv[])
 	char *line = NULL;
 	size_t len = 0;
 	ssize_t read;
-	mpz_t number, p, q, flag, __flag;
+	mpz_t number, p, q;
 	FILE *file = fopen(argv[1], "r");
 
 	if (file == NULL)
@@ -28,51 +32,48 @@ int main(int argc, char *argv[])
 		printf("File not found!\n");
 		return (1);
 	}
-	mpz_inits(number, p, q, flag, __flag, NULL);
-	while ((read = getline(&line, &len, file)) != -1)
-	{
-		mpz_set_str(number, line, 10);
-		mpz_set(q, number);
-		if (mpz_divisible_ui_p(q, 2) != 0)
-		{
-			mpz_cdiv_q_ui(q, q, 2);
-			gmp_printf("%Zd=%Zd*%d\n", number, q, 2);
-		}
-		else
-		{
-			mpz_sqrt(flag, number);
-			mpz_add_ui(flag, flag, 1);
-			mpz_set(__flag, flag);
-			for (mpz_set_ui(p, 3); mpz_cmp(p, flag) <= 0; mpz_add_ui(p, p, 2))
-			{
-				if (mpz_divisible_p(q, p) != 0)
-				{
-					mpz_cdiv_q(q, q, p);
-					gmp_printf("%Zd=%Zd*%Zd\n", number, q, p);
-					break;
-				}
-				mpz_add(__flag, flag, p);
-				if (mpz_divisible_p(q, __flag) != 0)
-				{
-					mpz_cdiv_q(q, q, __flag);
-					mpz_add(p, p, flag);
-					gmp_printf("%Zd=%Zd*%Zd\n", number, p, q);
-					break;
-				}
-				mpz_sub(__flag, flag, p);
-				if (mpz_divisible_p(q, __flag) != 0)
-				{
-					mpz_cdiv_q(q, q, __flag);
-					mpz_sub(p, flag, p);
-					gmp_printf("%Zd=%Zd*%Zd\n", number, p, q);
-					break;
-				}
-			}
-		}
-	}
-	if (line != NULL)
-		free(line);
-	mpz_clears(number, p, q, flag, __flag, NULL);
-	fclose(file);
-	return (0);
+
+mpz_inits(number, p, q, NULL);
+
+    while ((read = getline(&line, &len, file)) != -1)
+    {
+        // Convert line to a GMP integer
+        mpz_set_str(number, line, 10);
+
+        int found = 0;
+
+        // Start dividing from 2 to find the factors
+        for (mpz_set_ui(p, 2); mpz_cmp(p, number) < 0; mpz_add_ui(p, p, 1))
+        {
+            if (mpz_divisible_p(number, p) != 0)
+            {
+                mpz_cdiv_q(q, number, p);  // Calculate q = number / p
+
+                // Ensure the larger factor is printed first
+                if (mpz_cmp(p, q) > 0)
+                {
+                    gmp_printf("%Zd=%Zd*%Zd\n", number, p, q);
+                }
+                else
+                {
+                    gmp_printf("%Zd=%Zd*%Zd\n", number, q, p);
+                }
+
+                found = 1;
+                break;  // Stop after finding the first factor pair
+            }
+        }
+
+        // In case no factors are found (should not happen with valid inputs)
+        if (!found)
+        {
+            gmp_printf("%Zd is a prime number or could not be factorized\n", number);
+        }
+    }
+
+    if (line != NULL)
+        free(line);
+    mpz_clears(number, p, q, NULL);
+    fclose(file);
+    return (0);
 }
